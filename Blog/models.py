36f6bd45f717f321c12bb738b4c5e9e8cea77178
SAMUEL NNAJI <9jaflavzmedia@gmail.com>
2026-05-22 23:48:30 +0100

    last commit now

--- Blog/models.py
+++ Blog/models.py
@@ -99,7 +99,7 @@ class Music(models.Model):
 class News(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(default="No description")
-    news_tags = models.ManyToManyField(Tag, related_name= 'tags', help_text="Enter tags separated by commas", blank=True, null=True)
+    news_tags = models.ManyToManyField(Tag, related_name= 'tags', help_text="Enter tags separated by commas", blank=True)
     image = models.ImageField(upload_to='news_images/', blank=True, null=True)
     category = models.ManyToManyField(Category, related_name='News', blank=True)
     date_posted = models.DateTimeField(auto_now_add=True)
